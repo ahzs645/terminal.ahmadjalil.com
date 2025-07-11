7acb21246a67e670968f8d0f8ef93594ddcb01b4
Ahmad jalil <ahzs645@gmail.com>
2025-07-10 18:24:10 -0700

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/terminal.ahmadjalil.com/',
+  base: '/', // Changed from '/terminal.ahmadjalil.com/' to '/' for custom domain
   plugins: [
     react(),
     VitePWA({
